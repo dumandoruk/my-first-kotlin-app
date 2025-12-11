d155384d75d8589c124d73362fc5e436383f336b
Doruk Duman <77792846+dumandoruk@users.noreply.github.com>
2025-12-11 13:13:53 +0300

    Added layouts

--- app/src/main/java/com/dorukduman/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/dorukduman/artbookkotlin/ArtActivity.kt
@@ -1,23 +1,31 @@
 package com.dorukduman.artbookkotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.dorukduman.artbookkotlin.databinding.ActivityMainBinding
+import com.dorukduman.artbookkotlin.databinding.ActivityArtBinding
 
-class MainActivity : AppCompatActivity() {
+class ArtActivity : AppCompatActivity() {
 
-
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityArtBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityArtBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+
+    }
+        fun saveButtonClicked(view: View) {
+
         }
+        fun selectImage(view: View) {
+
+        }
+
 
 }
