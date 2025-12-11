fa0b712ae9db3a11c6c1e60c586d4ea00d256cc7
Doruk Duman <77792846+dumandoruk@users.noreply.github.com>
2025-12-11 14:18:37 +0300

    Added menu and editted themes

--- app/src/main/java/com/dorukduman/artbookkotlin/MainActivity.kt
+++ app/src/main/java/com/dorukduman/artbookkotlin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.dorukduman.artbookkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,7 +20,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //inflater
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.art_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId==R.id.add_art_item){
+            val intent= Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
